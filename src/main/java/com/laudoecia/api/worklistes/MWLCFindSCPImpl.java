184b8bc064a78db3008a2f609ef0234c725747ef
alankaiko <alamdfs@hotmail.com>
2020-03-13 17:46:44 -0300

    querys

--- src/main/java/com/laudoecia/api/worklistes/MWLCFindSCPImpl.java
+++ src/main/java/com/laudoecia/api/worklistes/MWLCFindSCPImpl.java
@@ -1,21 +1,21 @@
 package com.laudoecia.api.worklistes;
 
-import java.util.EnumSet;
-
 import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.Tag;
 import org.dcm4che3.data.UID;
 import org.dcm4che3.net.Association;
-import org.dcm4che3.net.QueryOption;
 import org.dcm4che3.net.Status;
-import org.dcm4che3.net.pdu.ExtendedNegotiation;
 import org.dcm4che3.net.pdu.PresentationContext;
 import org.dcm4che3.net.service.BasicCFindSCP;
+import org.dcm4che3.net.service.BasicQueryTask;
 import org.dcm4che3.net.service.DicomServiceException;
 import org.dcm4che3.net.service.QueryTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MWLCFindSCPImpl extends BasicCFindSCP {
-
+	private static final Logger LOG = LoggerFactory.getLogger(MWLCFindSCPImpl.class);
+	
+	
 	public MWLCFindSCPImpl() {
 		super(UID.ModalityWorklistInformationModelFIND);
 		System.out.println("ago vendo");
@@ -23,24 +23,10 @@ public class MWLCFindSCPImpl extends BasicCFindSCP {
 
 	@Override
 	protected QueryTask calculateMatches(Association as, PresentationContext pc, Attributes rq, Attributes keys)throws DicomServiceException {
-		System.out.println("opa deu certo");
 		try {
-			String cuid = rq.getString(Tag.AffectedSOPClassUID);
-			ExtendedNegotiation extNeg = as.getAAssociateAC().getExtNegotiationFor(cuid);
-			// TODO consider default Issuer Of Patient ID of Source AE
-			IDWithIssuer pid = IDWithIssuer.pidWithIssuer(keys, null);
-			IDWithIssuer[] pids = pid != null ? new IDWithIssuer[] { pid } : null;
-			// ArchiveDevice dev = (ArchiveDevice) as.getApplicationEntity().getDevice();
-			EnumSet<QueryOption> queryOpts = QueryOption.toOptions(extNeg);
-			QueryParam queryParam = new QueryParam();
-			queryParam.setCombinedDatetimeMatching(true);
-			queryParam.setFuzzySemanticMatching(queryOpts.contains(QueryOption.FUZZY));
-			// queryParam.setFuzzyStr(dev.getFuzzyStr());
+//			LOG.info("{}: Process MWL C-FIND RQ:\n{}", as, keys);
 			try {
-				//ModalityWorklistQuery query = (ModalityWorklistQuery) JNDIUtils.lookup(ModalityWorklistQuery.JNDI_NAME);
-				ModalityWorklistQuery query = new ModalityWorklistQueryBean();
-				query.findScheduledProcedureSteps(pids, keys, queryParam);
-				return new MWLQueryTaskImpl(as, pc, rq, keys, query);
+				return new BasicQueryTask(as, pc, rq, keys);
 			} catch (Exception e) {
 				throw new DicomServiceException(Status.UnableToProcess, e);
 			}
